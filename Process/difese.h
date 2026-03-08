6ed802ade10356f166fd0fa71953a45bdfe6c980
Ale <alessio.casertano2005@gmail.com>
2026-03-08 17:42:15 +0100

    Delete Process directory

--- Process/difese.h
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-// Funzione principale che gestisce le difese
-void gestione_difese(int pipe_out);
-
-// Funzione che gestisce il laser
-void gestione_laser(int pipe_out, struct Message pos_origin);
-
-
